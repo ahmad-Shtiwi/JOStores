eda7c1638faa7929d4fb2d97c5a28bbabfd48aaa
ahmad <ahmadshtiwi2000@gmail.com>
2022-06-21 18:59:39 +0300

    create section customer and change logo intro page and add arabic language

--- app/src/main/java/com/example/jostores/logInCustomers.java
+++ app/src/main/java/com/example/jostores/logInCustomers.java
@@ -2,13 +2,27 @@ package com.example.jostores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class logInCustomers extends AppCompatActivity {
-
+Button login,guest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in_customers);
+   guest=findViewById(R.id.guest_button_id);
+
+   guest.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View view) {
+           Intent  a=new Intent(logInCustomers.this,product.class);
+           startActivity(a);
+
+       }
+   });
+
     }
 }
